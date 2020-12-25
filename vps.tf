00196e15c664f0637d071dc5c984faf27d04584a
Bradley Stannard <bradley@breadnet.co.uk>
2020-12-25 02:56:26 +0000

    added firewall rules and moved ssh key

--- vps.tf
+++ vps.tf
@@ -1,48 +1,43 @@
 # https://jira.breadnet.co.uk/browse/CTOR-8
-variable "key_pair" {
-  type = map
-  default = {
-    public_key_path  = "/home/stannardb/.ssh/id_rsa.pub"
-    private_key_path = "/home/stannardb/.ssh/id_rsa"
-  }
+resource "openstack_compute_keypair_v2" "key" {
+  name       = "SSH"
+  public_key = file(var.pub_file_location)
 }
-# https://jira.breadnet.co.uk/browse/CTOR-8
-resource "openstack_compute_keypair_v2" "desktop" {
-  name = "desktop1"
 
-  public_key = "${file("${var.key_pair["public_key_path"]}")}"
-}
-#Add firewall rules for inbound
 # https://jira.breadnet.co.uk/browse/CTOR-7
-resource "openstack_networking_secgroup_rule_v2" "" {
-  direction = "Ingress"
-  ethertype = ""
-  security_group_id = ""
+resource "openstack_networking_secgroup_v2" "inbound" {
+  name        = "inbound"
+  description = "Inbound rules"
 }
 
-#https://jira.breadnet.co.uk/browse/CTOR-4
+# https://jira.breadnet.co.uk/browse/CTOR-7
+resource "openstack_networking_secgroup_rule_v2" "inbound_ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  remote_ip_prefix  = var.ip
+  port_range_min    = 22
+  port_range_max    = 22
+  security_group_id = openstack_networking_secgroup_v2.inbound.id
+}
+
+# https://jira.breadnet.co.uk/browse/CTOR-7
+resource "openstack_networking_secgroup_rule_v2" "icmp" {
+  direction = "ingress"
+  ethertype = "IPv4"
+  remote_ip_prefix = var.ip
+  protocol = "icmp"
+  security_group_id = openstack_networking_secgroup_v2.inbound.id
+}
+
+# https://jira.breadnet.co.uk/browse/CTOR-4
 resource "openstack_compute_instance_v2" "torrentbox" {
-  name        = "torrent-box"
-  flavor_name = "s1-2"
-  key_pair    = openstack_compute_keypair_v2.desktop.name
-  image_name  = "Ubuntu 18.04"
+  name            = "torrent-box"
+  flavor_name     = "s1-2"
+  key_pair        = openstack_compute_keypair_v2.key.name
+  image_name      = "Ubuntu 18.04"
+  security_groups = [openstack_networking_secgroup_v2.inbound.id]
   network {
     name = "Ext-Net"
   }
 }
-
-# Should remove but is being used for testing
-resource "cloudflare_record" "torrent" {
-  name    = "torrent"
-  type    = "A"
-  zone_id = var.zone_id
-  proxied = "false"
-  value   = openstack_compute_instance_v2.torrentbox.network[0].fixed_ip_v4
-}
-
-#output "fixed_ip_v4" {
-#value = openstack_compute_instance_v2.torrentbox[count.index].network[0].fixed_ip_v4
-#}
-#output "tfid" {
-# value = openstack_compute_instance_v2.torrentbox[count.index].id
-#}
